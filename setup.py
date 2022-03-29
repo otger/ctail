176eb0e627ba8f03cd0d03c92aab0342c1041de4
Otger <otger@ifae.es>
2022-03-29 10:08:07 +0200

    Forgot to increase version on setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ctail",
-    version="0.0.2",
+    version="0.0.3",
     author="Otger Ballester",
     author_email="otger@gmail.com",
     description="A colored tail",
